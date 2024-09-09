46b40e2fec126bd575ec86a7deff570aa47f459d
tony-huaulme <tonyhuaulme@gmail.com>
2024-09-09 21:52:52 +0200

    login page

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -3,50 +3,55 @@
     <div class="flex h-screen justify-center md:p-5">
 
         <!-- Right Section: Placeholder for Login Form -->
-        <div class="md:w-1/2 h-fill w-fill md:max-w-xl flex justify-center relative" style="background-color: var(--p-content-background);">
-            <!-- Replace this with your login form -->
-            <div class="w-2/3 max-w-md mt-10 flex flex-column justify-center ">
-                <i class="pi pi-box text-8xl self-center mb-5"></i>
-                <h1 class="text-2xl font-semibold text-gray-700 text-center mb-6">Welcome to MyAssembly.co!</h1>
+        <div class="md:w-1/2 h-fill w-fill md:max-w-xl flex flex-column justify-center relative" style="background-color: var(--p-content-background);">
+            
+            <div class="w-fill flex flex-column items-center">
 
-                <div v-if="signupIndex == 0" class="flex flex-col gap-2">
-                    <label for="username">Work email address</label>
-                    <InputText id="username" v-model="email" />
-                    <small v-if="emailWarn" class="p-error">Invalid email format</small>
-                </div>
+                <!-- Replace this with your login form -->
+                <div class="w-2/3 max-w-md mt-10 flex flex-column justify-center ">
+                    <i class="pi pi-box text-8xl self-center mb-5"></i>
+                    <h1 class="text-2xl font-semibold text-gray-700 text-center mb-6">{{ signupPage ? 'Welcome to MyAssembly.co!' : 'Log into my account' }}</h1>
 
-                <div v-else class="flex flex-col gap-2">
-                    <label for="username">Create new password</label>
-                    <InputText id="username" v-model="password" type="password" />
-                </div>
-
-
-                <Button :label="signupIndex === 0 ? 'Continue' : 'Create Account'" @click="goToNextSignupStep"
-                    class="mt-6 w-fill"></Button>
-                <div v-if="signupIndex == 0">
-                    <Divider align="center" type="solid" style="margin: 48px 0px;">
-                        <RouterLink :to="'/dashboard/Projects'"><b>OR</b></RouterLink>
-                    </Divider>
-
-                    <div>
-                        <div id="g_id_onload"
-                            data-client_id="93244253552-q2qn4t6vjerpf6mj1qgfduf69agfulas.apps.googleusercontent.com"
-                            data-callback="handleCredentialResponse">
-                        </div>
-                        <div class="g_id_signin" data-type="standard"></div>
+                    <div v-if="signupIndex == 0" class="flex flex-col gap-2">
+                        <label for="username">Work email address</label>
+                        <InputText @keydown.enter="goToNextSignupStep" id="username" v-model="email" />
+                        <small v-if="emailWarn" class="p-error">Invalid email format</small>
                     </div>
+
+                    <div v-else class="flex flex-col gap-2">
+                        <label for="password">{{ signupPage ? 'Create new password' : 'Password' }}</label>
+                        <InputText @keydown.enter="goToNextSignupStep" id="password" v-model="password" type="password" />
+                    </div>
+
+
+                    <Button :label="signupIndex === 0 ? 'Continue' :  signupPage ? 'Create Account' : 'Login'" @click="goToNextSignupStep"
+                        class="mt-6 w-fill"></Button>
+                    
                 </div>
-
-                <!-- Navigation -->
-                <i class="pi pi-times absolute top-5 right-5 cursor-pointer text-xl" @click="$router.push('/')"></i>
-                <i v-if="signupIndex > 0" @click="signupIndex = signupIndex - 1"
-                    class="pi pi-angle-left absolute top-5 left-5 cursor-pointer text-xl">
-                </i>
-
-                <p>Have an accout? <RouterLink :to="'/signin'">SignIn</RouterLink>
-                </p>
             </div>
+
+            <div>
+                <Divider align="center" type="solid" style="margin: 48px 0px;">
+                    <RouterLink :to="'/dashboard/Projects'"><b>OR</b></RouterLink>
+                </Divider>
+
+                <div>
+                    <div id="g_id_onload"
+                        data-client_id="93244253552-q2qn4t6vjerpf6mj1qgfduf69agfulas.apps.googleusercontent.com"
+                        data-callback="handleCredentialResponse">
+                    </div>
+                    <div class="g_id_signin" data-type="standard"></div>
+                </div>
+            </div>
+            <p class="flex flex-column items-center">{{ signupPage ?  'Have an accout? ' : "Don't have an account? " }}<a class="underline" @click="signupPage = !signupPage">{{ signupPage ? ' SignIn' : ' SignUp' }}</a></p>
+            <!-- Navigation -->
+            <i class="pi pi-times absolute top-5 right-5 cursor-pointer text-xl" @click="$router.push('/')"></i>
+            <i v-if="signupIndex > 0" @click="signupIndex = signupIndex - 1"
+                class="pi pi-angle-left absolute top-5 left-5 cursor-pointer text-xl">
+            </i>
+
         </div>
+
     </div>
 </template>
 
@@ -58,14 +63,20 @@ import Button from 'primevue/button';
 import Divider from 'primevue/divider';
 import { ref } from 'vue';
 
+const signupPage = ref(1);
+
 const email = ref('');
 const password = ref('');
 const signupIndex = ref(0);
 const emailWarn = ref(false);
 
+
+const passwordInputElement = document.getElementById('password');
+
 function goToNextSignupStep() {
     if (signupIndex.value === 0 && /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email.value)) {
         signupIndex.value = signupIndex.value + 1;
+        passwordInputElement.focus();
         emailWarn.value = false;
         return;
     } else {
@@ -103,7 +114,9 @@ function handleCredentialResponse(response) {
 onMounted(() => {
 
     // Google Sign-In
+    const emailInputElement = document.getElementById('username');
 
+    emailInputElement.focus();
     // Initialize the Google sign-in button
     window.google.accounts.id.initialize({
     client_id: '93244253552-q2qn4t6vjerpf6mj1qgfduf69agfulas.apps.googleusercontent.com',
@@ -116,3 +129,10 @@ onMounted(() => {
     );
 });
 </script>
+<style>
+.S9gUrf-YoZ4jf{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+</style>
