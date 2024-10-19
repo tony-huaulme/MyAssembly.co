f92dd25b0797f3c58f7db40c9938eebfc5328c96
tony-huaulme <tonyhuaulme@gmail.com>
2024-10-19 12:01:06 +0200

    dashbord create project

--- src/components/Dashboard/ProjectsTable.vue
+++ src/components/Dashboard/ProjectsTable.vue
@@ -28,7 +28,7 @@
             <!-- Empty and Loading Templates -->
             <template #empty>
                 Create your first project now !
-                <div id="pv_id_1_1_0" class="p-menu-item" role="menuitem" aria-label="New" data-pc-section="item" data-p-focused="false" data-p-disabled="false"><div class="p-menu-item-content" data-pc-section="itemcontent"><a class="flex items-center blink p-menu-item-link" onclick="this.classList.remove('blink')" tabindex="-1" aria-hidden="true" data-pc-section="itemlink"><span class="pi pi-plus"></span><span class="ml-2">New</span><!--v-if--><!--v-if--></a></div></div>
+                <div id="pv_id_1_1_0" @click="clickCreateProject" class="p-menu-item" role="menuitem" aria-label="New" data-pc-section="item" data-p-focused="false" data-p-disabled="false"><div class="p-menu-item-content" data-pc-section="itemcontent"><a class="flex items-center blink p-menu-item-link" tabindex="-1" aria-hidden="true" data-pc-section="itemlink"><span class="pi pi-plus"></span><span class="ml-2">New</span><!--v-if--><!--v-if--></a></div></div>
             </template>
             <template #loading>Loading project data. Please wait.</template>
 
@@ -76,8 +76,8 @@
             </Column>
         </DataTable>
         <div v-else>
-            No projects found.
-            Create a new project to get started.
+            Create your first project now !
+            <div id="pv_id_1_1_0" @click="clickCreateProject" class="p-menu-item" role="menuitem" aria-label="New" data-pc-section="item" data-p-focused="false" data-p-disabled="false"><div class="p-menu-item-content" data-pc-section="itemcontent"><a class="flex items-center blink p-menu-item-link" tabindex="-1" aria-hidden="true" data-pc-section="itemlink"><span class="pi pi-plus"></span><span class="ml-2">New</span><!--v-if--><!--v-if--></a></div></div>
         </div>
     </div>
     <!-- <ShareDemoModal /> -->
@@ -98,6 +98,9 @@ import { useRouter } from 'vue-router';
 
 import { FilterMatchMode, FilterOperator } from '@primevue/core/api'; // Correct import for PrimeVue API filters
 
+function clickCreateProject() {
+    document.getElementById('createProjectButton').click();
+}
 
 const router = useRouter();
 
